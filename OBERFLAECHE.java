14eeca2835e93baf9c1a135426688190193101c5
paulaoe <paula.oexl@gmail.com>
2022-05-24 09:57:00 +0200

    keine fehler mehr

--- OBERFLAECHE.java
+++ OBERFLAECHE.java
@@ -6,7 +6,7 @@
  * @author (PMD) 
  * @version (eine Versionsnummer oder ein Datum)
  */
-<<<<<<< HEAD
+
 import javax.swing.*;
 import java.awt.event.*;
 import java.awt.event.ActionEvent;
@@ -16,10 +16,9 @@ import javax.swing.JOptionPane;
 import javax.swing.table.DefaultTableCellRenderer;
 import java.awt.Component;
 
-public class OBERFLAECHE
-=======
+
 public class OBERFLAECHE extends JFrame
->>>>>>> 880d4e265c29686ec10ea55d87af5ea1301341ec
+
 {
     // Instanzvariablen - ersetzen Sie das folgende Beispiel mit Ihren Variablen
     private int x;
